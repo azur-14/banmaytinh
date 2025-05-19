984cabab2257cf29dda0a7c4e8793f868ea9b2c1
azur-14 <hieungan0906@gmail.com>
2025-05-20 02:06:39 +0700

    build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -60,7 +60,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1acebcc4ea015b6a13c309a31229db11
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "4e9b04b8904340642e66e65de59408e3",
+"flutter_bootstrap.js": "26c8e89c57c4b360e559e131fc43a37e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
